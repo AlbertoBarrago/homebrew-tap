e96e1134dbfc7d81c45045c21ba007f0c95d28b5
GitHub Action <action@github.com>
2026-01-29 10:14:07 +0000

    Update wir to 1.0.9

--- Formula/wir.rb
+++ Formula/wir.rb
@@ -1,9 +1,9 @@
 class Wir < Formula
   desc "What Is Running - Port and Process Inspector"
   homepage "https://github.com/AlbertoBarrago/Wir"
-  url "https://github.com/AlbertoBarrago/Wir/releases/download/v1.0.8/wir-1.0.8.tar.gz"
-  sha256 "bc0e7082134da2180d838ae5445d2aee7ff3650fbe3c1b72d08378cfcb1304a5"
-  version "1.0.8"
+  url "https://github.com/AlbertoBarrago/Wir/releases/download/v1.0.9/wir-1.0.9.tar.gz"
+  sha256 "39d50cd96dc83b7e70cd726d6621325732100de8b18059b42e9fa2dd57b4d09f"
+  version "1.0.9"
   license "MIT"
 
   def install
